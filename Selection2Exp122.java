6d9ddc02d9bf4f056ff7ea0ebadc3e4c8c78fa4c
naddiapp <nadiapramudita43@gmail.com>
2023-10-11 23:58:20 +0700

    Selection2Exp122

--- Selection2Exp122.java
+++ Selection2Exp122.java
@@ -6,10 +6,13 @@ public class Selection2Exp122 {
         System.out.print("Input year= ");
         year = input22.nextInt();
 
-        if (year%4 == 0)
-            if (year%100 != 0)
+        if (year%4 == 0) {
+            if (year%100 != 0) {
                  System.out.println("Leap year");
-        else
+            }else {
+                 System.out.println("Not a leap year");
+            }
+        } else
             System.out.println("Not a leap year");
-        }
     }
+}
