bd519a00084ff34d55c27df700a5881bc90be646
naddiapp <nadiapramudita43@gmail.com>
2023-10-12 19:25:25 +0700

    Selection2Exp222

--- Selection2Exp122.java
+++ Selection2Exp122.java
@@ -8,6 +8,7 @@ public class Selection2Exp122 {
 
         if (year%4 == 0) {
             if (year%100 != 0) {
+                if ((year%400) == 0);
                  System.out.println("Leap year");
             }else {
                  System.out.println("Not a leap year");
--- Selection2Exp222.java
+++ Selection2Exp222.java
@@ -4,7 +4,6 @@ public class Selection2Exp222 {
     public static void main(String[] args) {
         Scanner input22 = new Scanner(System.in);
         int angle1, angle2, angle3, totalAngle;
-        int total;
 
         System.out.println("Input angle1 = ");
         angle1 = input22.nextInt();
@@ -14,12 +13,17 @@ public class Selection2Exp222 {
         angle3 = input22.nextInt();
         totalAngle = angle1+angle2+angle3;
 
-        if(totalAngle == 180)
-            if(angle1==90 || angle2==90 || angle3==90)
+        if(totalAngle == 180) {
+            if((angle1==90 || angle2==90 || angle3==90)) {
                 System.out.println("Right triangle");
-            else
-                System.out.println("Not a right triangle");
-            else    
-                System.out.println("Not a triangle");        
-    }
-}
+            }else if(angle1==angle2&&angle2==angle3) {
+                System.out.println("Equilateral triangle");
+            }else if (angle1==angle2||angle1==angle3||angle2==angle3) {
+                System.out.println("Isosceles triangle"); 
+            }else 
+                System.out.println("Any triangle"); 
+            }else         
+                System.out.println("Not a triangle");
+            }   
+         }
+        
